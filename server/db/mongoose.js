c9381fd89cef9c400298eaedff469f321d71077a
Dheeraj Chaudhary <Dheeraj.Chaudhary@contractor.hallmark.com>
2018-02-11 18:10:45 -0600

    mONGO db uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,13 +2,13 @@
  * @Author: Dheeraj Chaudhary 
  * @Date: 2018-02-11 14:12:11 
  * @Last Modified by: Dheeraj.Chaudhary@contractor.hallmark.com
- * @Last Modified time: 2018-02-11 14:33:17
+ * @Last Modified time: 2018-02-11 18:10:30
  */
 var mongoose = require('mongoose');
 
 //Connect to Database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose: mongoose
